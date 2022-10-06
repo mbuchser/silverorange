da72dbfea4ee8bba16bc9ec58e12d86c073f0743
Marc Buchser <buero@marcbuchser.ch>
2022-10-06 14:10:43 -0600

    first draft layout implemented with assets and string resources

--- android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/VideoPlayer/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.silverorange.videoplayer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
